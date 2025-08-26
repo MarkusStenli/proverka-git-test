e9424a38331e4592f5f9dfcbfce5969c7b8a9f12
Markus <marku.stenli.47@gmail.com>
2025-08-26 22:45:26 +0300

    решена задача task 12345 забыл добавить в общий объем

--- src/main/java/ru/otus/java/basic/GitApp.java
+++ src/main/java/ru/otus/java/basic/GitApp.java
@@ -2,11 +2,13 @@ package ru.otus.java.basic;
 
 public class GitApp {
     public static void main(String[] args) {
+        drawSquare();
         System.out.println(1);
         System.out.println("Hello World!!!");
         System.out.println(2);
         int a = 10;
         System.out.println(a);
+        drawSquare();
     }
 
     public static void drawSquare() {
