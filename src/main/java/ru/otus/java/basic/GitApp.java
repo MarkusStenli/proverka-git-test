a8a61cce0a019a5a1b954e50cc13e68df0be12b2
Markus <marku.stenli.47@gmail.com>
2025-08-26 23:36:32 +0300

    вывод результата в консоль

--- src/main/java/ru/otus/java/basic/GitApp.java
+++ src/main/java/ru/otus/java/basic/GitApp.java
@@ -3,5 +3,12 @@ package ru.otus.java.basic;
 public class GitApp {
     public static void main(String[] args) {
         System.out.println("калькулятор:начало");
+
+        System.out.println("суммирование:");
+        int sumA = 1;
+        int sumB = 2;
+        int sumResult = sumA + sumB;
+        System.out.println(sumA + "+" + sumB + "=" + sumResult);
+
     }
 }
