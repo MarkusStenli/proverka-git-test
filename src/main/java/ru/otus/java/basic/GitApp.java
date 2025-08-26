f0a28ad503cea5bc6df2e2ce8a5d4458b3ca92cc
Markus <marku.stenli.47@gmail.com>
2025-08-26 22:30:50 +0300

    научился печатать в консоль

--- src/main/java/ru/otus/java/basic/GitApp.java
+++ src/main/java/ru/otus/java/basic/GitApp.java
@@ -2,6 +2,8 @@ package ru.otus.java.basic;
 
 public class GitApp {
     public static void main(String[] args) {
+        System.out.println(1);
         System.out.println("Hello World!!!");
+        System.out.println(2);
     }
 }
