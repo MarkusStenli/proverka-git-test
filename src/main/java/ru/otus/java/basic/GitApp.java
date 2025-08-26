13d8f758b943881279393d42e86c9510bcf60272
Markus <marku.stenli.47@gmail.com>
2025-08-26 23:32:50 +0300

    калькулятор

--- src/main/java/ru/otus/java/basic/GitApp.java
+++ src/main/java/ru/otus/java/basic/GitApp.java
@@ -2,27 +2,6 @@ package ru.otus.java.basic;
 
 public class GitApp {
     public static void main(String[] args) {
-        drawSquare();
-        System.out.println(1);
-        System.out.println("Hello World!!!");
-        System.out.println(2);
-        int a = 10;
-        System.out.println(a);
-        drawSquare();
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        int n = 20;
-        n++;
-        System.out.println(n);
-    }
-
-    public static void drawSquare() {
-        System.out.println("----");
-        System.out.println("-  -");
-        System.out.println("-  -");
-        System.out.println("----");
-        byte m = 127;
-        System.out.println(m);
+        System.out.println("калькулятор:начало");
     }
 }
