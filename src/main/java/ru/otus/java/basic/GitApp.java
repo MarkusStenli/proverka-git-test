30f8467bb296a45b2b403c3c58a4663d61c2f7f4
Markus <marku.stenli.47@gmail.com>
2025-08-26 22:46:46 +0300

    решена задача с переменной n

--- src/main/java/ru/otus/java/basic/GitApp.java
+++ src/main/java/ru/otus/java/basic/GitApp.java
@@ -9,6 +9,12 @@ public class GitApp {
         int a = 10;
         System.out.println(a);
         drawSquare();
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        int n = 20;
+        n++;
+        System.out.println(n);
     }
 
     public static void drawSquare() {
