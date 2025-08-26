04dd8fddd240395ab34c0f0e601059d7102c4103
Markus <marku.stenli.47@gmail.com>
2025-08-26 23:39:45 +0300

    задача вычетания решена

--- src/main/java/ru/otus/java/basic/GitApp.java
+++ src/main/java/ru/otus/java/basic/GitApp.java
@@ -3,5 +3,10 @@ package ru.otus.java.basic;
 public class GitApp {
     public static void main(String[] args) {
         System.out.println("калькулятор:начало");
+
+        int subtractA = 10;
+        int subtractB = 10;
+        int subtractResult = subtractA - subtractB;
+
     }
 }
